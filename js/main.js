b6b875dfb1d20cac63cbba51b4daea00556231f1
Maria Friedemann <friedemann.maria@gmail.com>
2023-05-23 22:59:15 +0200

    js functionality added

--- js/main.js
+++ js/main.js
@@ -1,40 +1,3 @@
-const music = new Audio();
-
-const answerButtons = document.querySelectorAll(".answer-button");
-
-class Game {
-  constructor() {
-    this.player = null;
-    this.points = 0;
-  }
-
-  start() {
-    const playMusicBtn = document.querySelector(".spotify-button");
-
-    function playMusic() {
-      const audio = new Audio(
-        "./songs/01_Scorpions - Rock You Like A Hurricane.mp3"
-      );
-      audio.play();
-    }
-
-    playMusicBtn.addEventListener("click", playMusic);
-  }
-}
-/*
-const interpretText = document.querySelector("#interpret"); // where to put this function???
-    
-    function displaySongInfo() {
-      const interpret = new Interpret('songs.interpret');
-      interpret.textContent();
-
-      */ 
-
-   // document.addEventListener("click", displaySongInfo);  ???? (click on PlayMusicBtn doesnt work)
-
-const newGame = new Game();
-newGame.start();
-
 const songs = [
   {
     audio: "./songs/01_Scorpions - Rock You Like A Hurricane.mp3",
@@ -88,5 +51,116 @@ const songs = [
   },
 ];
 
+for(let i = 0, i < songs.length, i++) {
+  
+}
 
-//updatePoints();
+
+
+class Game {
+  constructor() {
+    this.player = null;
+    this.points = 0;
+    this.round = 1;
+   // this.song = songs[];
+  }
+
+  start() {
+    const playMusicBtn = document.querySelector(".spotify-button");
+    // const pauseMusicBtn = document.querySelector(".spotify-button");
+
+    //playMusicBtn.addEventListener("click", this.playMusic); // invokes play music method
+    playMusicBtn.addEventListener("click", this.displaySongInfo); // invokes play music method
+    playMusicBtn.addEventListener("click", this.displayAnswers); // invokes play music method
+
+    const answerBtnArr = document.querySelectorAll(".answer-button"); // invokes checkAnswers method
+    answerBtnArr.forEach((button) => {
+      button.addEventListener("click", this.checkAnswers)
+    })
+
+    answerBtnArr.forEach((button) => {
+      button.addEventListener("click", (event) => {
+        const currentSong = songs[0];
+        const correctAnswer = currentSong.correctAnswer;
+
+        if (event.target.innerHTML === 'Yay! Correct answer!') {
+          this.updatePoints();
+        }
+      });
+    });
+
+  
+  }
+ 
+  
+
+  playMusic() {
+    const currentSong = songs[0];
+    const audio = new Audio(currentSong.audio);
+
+    audio.play(); 
+  }
+    
+  displaySongInfo() {
+    const currentSong = songs[0];
+    const songInfo = document.querySelector(".song-info-box"); 
+    songInfo.textContent = `Interpret: ${currentSong.interpret} Song: ${currentSong.songTitle}`;
+  }
+
+  displayAnswers() {
+    const currentSong = songs[0];
+    const answerOption1 = document.querySelector("#answer1");
+    const answerOption2 = document.querySelector("#answer2");
+    const answerOption3 = document.querySelector("#answer3");
+    const answerOption4 = document.querySelector("#answer4");
+
+
+    answerOption1.textContent = `${currentSong.answer1}`;
+    answerOption2.textContent = `${currentSong.answer2}`;
+    answerOption3.textContent = `${currentSong.answer3}`;
+    answerOption4.textContent = `${currentSong.answer4}`;
+
+  }
+
+  checkAnswers(event) {
+    const currentSong = songs[0];
+    const correctAnswer = currentSong.correctAnswer;
+ 
+    if(event.target.innerHTML === correctAnswer) {
+  
+      event.target.innerHTML = 'Yay! Correct answer!';
+      event.target.style.color = 'green';
+      this.points++;
+
+    } else {
+      event.target.innerHTML = 'Sorry! Wrong answer!';
+      event.target.style.color = 'red';
+    }
+
+    }
+
+  updatePoints() {
+    this.points++;
+  } 
+
+  moveToNextRound() {
+    if (this.checkAnswers && this.updatePoints) {
+      //start new Round
+    }
+  }
+
+}
+
+const newGame = new Game();
+newGame.start();
+
+
+// replicating code for all 5 rounds
+
+
+/*
+ function pauseMusic() {
+      const audio = new Audio(
+        "./songs/01_Scorpions - Rock You Like A Hurricane.mp3"
+      );
+      audio.pause(); */
